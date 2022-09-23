3efedc31a1819afb948a096eb12654a25ffdf92d
Tokenizers <romain.barraud1@gmail.com>
2022-09-23 16:26:07 +0200

    add gatsby cloud plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,7 @@ module.exports = {
     siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: [
+    "gatsby-plugin-gatsby-cloud",
     "gatsby-plugin-less",
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
